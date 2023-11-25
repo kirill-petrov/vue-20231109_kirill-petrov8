a6674d453569653f33c7e981702b7c27a5ed923f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-11-25 22:49:19 +0300

    Added solution for task: CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,15 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+}).mount('#app');
